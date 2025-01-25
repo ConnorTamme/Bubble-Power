895df6e782d796da0c8ce4e93c595cbf459b8d7e
Anthony C Blower <acb286@usask.ca>
2025-01-25 11:45:23 -0600

    fumble 1 - wasn't saved prior to commit

--- player.gd
+++ player.gd
@@ -7,7 +7,6 @@ class_name Player
 #@export var shotspeed:Vector2 = Vector2(200,0)
 var shotspeed = 3000
 @export var playerID = 1
-@export var healthPerShot = 1
 @export var hitDamage = 20
 
 @export var sprintFactor = 1.5
@@ -15,20 +14,22 @@ var shotspeed = 3000
 var maxHealth = 100
 var screen_size
 var DEBUG: bool = false;
-#var velocity = Vector2.ZERO
 var shotDirection = 0
 var health = maxHealth
 
 # Variables for diverse cast
-var walkAnimName
-var idleAnimName
-var rightInput
-var leftInput
-var upInput 
-var downInput
-var shootInput
-var sprintKey
-var sneakKey
+var walkAnimRight = "walk_right"
+var walkAnimLeft = "walk_left"
+var walkAnimName = "walk"
+var idleAnimName = "idle"
+
+var rightInput = "p1_right"
+var leftInput = "p1_left"
+var upInput = "p1_up"
+var downInput = "p1_down"
+var shootInput = "p1_shoot"
+var sprintKey =  "p1_sprint"
+var sneakKey =  "p1_sneak"
 
 # Crosshair variables
 @export var crosshairDist = 200
@@ -43,56 +44,38 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	crossHair = crossHairObj.instantiate();
 	add_child(crossHair)
-
 	
-	# Connect Inputs based on player ID
-	rightInput = "p" + str(playerID) + "_right"
-	leftInput = "p" + str(playerID) + "_left"
-	upInput = "p" + str(playerID) + "_up"
-	downInput = "p" + str(playerID) + "_down"
-	shootInput = "p" + str(playerID) + "_shoot"
-	sprintKey =  "p" + str(playerID) + "_sprint"
-	sneakKey =  "p" + str(playerID) + "_sneak"
-	
-	# Connect character animations
-	walkAnimName = "walk"
-	idleAnimName = "idle"
 	$AnimatedSprite2D.animation = idleAnimName
 	$AnimatedSprite2D.play()
 	
-	match playerID:
-		1: position = Vector2(screen_size.x/4, screen_size.y/4)
-		2: position = Vector2(3*screen_size.x/4, screen_size.y/4)
-		3: position = Vector2(screen_size.x/4, 3*screen_size.y/4)
-		4: position = Vector2(3*screen_size.x/4, 3*screen_size.y/4)
-	
+	# Where player starts on screen
+	position = Vector2(screen_size.x/4, screen_size.y/4)
+
 	# Connect Health bar
 	$HealthBar.max_value = maxHealth
 	$HealthBar.value = health
+	
+	#throw error if no bullet
 	if(bullet == null): # Move this so it is not done every time.
 			push_error("No bullet assigned to Player #", playerID)
 
-	
-	# Set layer and mask for player and projectiles
-	#for i in range(1, 5):
-		#if(i == playerID):
-			#set_collision_layer_value(i, true)
-			#set_collision_mask_value(i, false)
-		#else:
-			#set_collision_layer_value(i, false)
-			#set_collision_mask_value(i, true)
+
 func checkHealth():
 	if(health < 0):
 		health = 0;
 	$HealthBar.value = health 
 	if(health <= 0):
 		_die()
+
+
 func movePlayer(delta):
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed(rightInput):
 		velocity.x += 1
+		walkAnimName = walkAnimLeft
 	elif Input.is_action_pressed(leftInput):
 		velocity.x -= 1
+		walkAnimName = walkAnimRight
 	
 	if Input.is_action_pressed(upInput):
 		velocity.y -= 1
@@ -115,9 +98,9 @@ func movePlayer(delta):
 	else:
 		$AnimatedSprite2D.animation = idleAnimName
 	
-	position += velocity * delta
-	#move_and_slide(velocity)
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position += velocity * delta # Adjust for clock speed
+	position = position.clamp(Vector2.ZERO, screen_size) # stop player from leaving screen.
+	
 	
 # Draw a crosshair at the vector, normalized from user times a distance.
 func getAttackAngle():
@@ -153,10 +136,6 @@ func _process(delta):
 			#shot.set_collision_layer_value(playerID, false)
 			add_child(shot)
 		$ShotTimer.start(shotDelay);
-	
-	#Sprite Animation
-	#if(velocity.x != 0):
-		#$AnimatedSprite2D.flip_h = velocity.x < 0
 
 func _die():
 	print("ARRRGH!!! PLAYER ", str(playerID), " has died!")
