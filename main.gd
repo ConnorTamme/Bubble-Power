942c3c344fcf8ff27164e0ba3fb3ec101c419cac
Adhyan <adhyanpatel@gmail.com>
2025-01-25 19:03:55 -0600

    enemies now spawn

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start()
+	$MobTimer.start()
 	pass # Replace with function body.
 
 
@@ -13,17 +13,28 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_timer_timeout() -> void:
-	var random = RandomNumberGenerator.new()
-	random.randomize()
-	
-	DisplayServer.window_get_size()
-	var sz = get_viewport().get_visible_rect().size
-	
-	var x_pos = (randi() % int(sz.x))
-	var y_pos = (randi() % int(sz.y))
+
+func _on_mob_timer_timeout() -> void:
+	# Create a new instance of the Mob scene.
 	var e = enemy.instantiate()
-	e.position.x = x_pos
-	e.position.y = y_pos
+
+	# Choose a random location on Path2D.
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	e.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	#direction += randf_range(-PI / 4, PI / 4)
+	#e.rotation = direction
+
+ 		# Choose the velocity for the mob.
+	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	#e.linear_velocity = velocity.rotated(direction)
+
+	# Spawn the mob by adding it to the Main scene.
 	add_child(e)
-	$Timer.start()
