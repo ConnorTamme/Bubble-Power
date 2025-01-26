5fb4cfa99be7b11451974ef86c60c87191ec1359
Anthony C Blower <acb286@usask.ca>
2025-01-26 14:05:24 -0600

    Tweaked a few things made a theme for buttons

--- start_screen.gd
+++ start_screen.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Title.text = "[center]%s[/center]" % $Title.text
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
